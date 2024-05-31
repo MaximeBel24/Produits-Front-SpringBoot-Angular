8502a80ff13a1ac72dda52bbf54679217505187c
Maxime <maxime.b2494@gmail.com>
2024-05-31 14:27:38 +0200

    Ajout de Categorie au Produit

--- src/app/add-produits/add-produits.component.ts
+++ src/app/add-produits/add-produits.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Produit } from '../model/produit.model';
 import { ProduitService } from '../produit.service';
+import { Categorie } from '../model/categorie.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-produits',
@@ -10,15 +12,26 @@ import { ProduitService } from '../produit.service';
 export class AddProduitsComponent implements OnInit {
 
   newProduit = new Produit();
+  categories! : Categorie[];
 
-  constructor(private produitService: ProduitService) {}
+  newIdCat! : number;
+  newCategorie! : Categorie;
+
+  constructor(
+    private produitService: ProduitService,
+    private router : Router
+  ) {}
 
   addProduit() {
     // console.log(this.newProduit);
+    this.newCategorie = this.produitService.consulterCategorie(this.newIdCat);
+    this.newProduit.categorie = this.newCategorie;
     this.produitService.ajouterProduit(this.newProduit);
+    this.router.navigate(['produits']);
   }
 
   ngOnInit(): void {
+    this.categories = this.produitService.listeCategories();
   }
 
 }
--- src/app/update-produit/update-produit.component.ts
+++ src/app/update-produit/update-produit.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Produit } from '../model/produit.model';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ProduitService } from '../produit.service';
+import { Categorie } from '../model/categorie.model';
 
 @Component({
   selector: 'app-update-produit',
@@ -11,6 +12,8 @@ import { ProduitService } from '../produit.service';
 export class UpdateProduitComponent implements OnInit {
 
   currentProduit = new Produit();
+  categories! : Categorie[];
+  updatedCatId! : number;
 
   constructor(
     private activatedRoute : ActivatedRoute,
@@ -19,13 +22,15 @@ export class UpdateProduitComponent implements OnInit {
   ) { }
 
   updateProduit() {
+    this.currentProduit.categorie=this.produitService.consulterCategorie(this.updatedCatId);
     this.produitService.updateProduit(this.currentProduit);
     this.router.navigate(['produits']);
   }
 
   ngOnInit(): void {
+    this.categories = this.produitService.listeCategories();
     this.currentProduit = this.produitService.consulterProduit(this.activatedRoute.snapshot.params['id']);
-    console.log(this.currentProduit);
+    this.updatedCatId = this.currentProduit.categorie.idCat;
   }
 
 }
